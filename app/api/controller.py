dc227576c758979e1e28e480d3b3ecfcba7aec2b
YangYuenan <18810650391@139.com>
2021-10-11 20:04:43 +0800

    详细信息图表改为大数据视图

--- app/api/controller.py
+++ app/api/controller.py
@@ -12,15 +12,82 @@ from app.models import ItemStatus
 
 
 # 获取项目状态图表
+# def get_item_echarts(rq):
+#     if rq.method == 'POST':
+#         status_list = []
+#         rx_speed_list = []
+#         tx_speed_list = []
+#         date_list = []
+#         avg_list = []
+#         avg_rx_list = []
+#         avg_tx_list = []
+#         item_id = rq.form.get('item_id')
+#         date_ware = rq.form.get('date_ware')
+#         warning_value = rq.form.get('warning_value')
+#         start_date, end_date = date_ware.split('-')
+#         query = ItemStatus.select(ItemStatus.item_name, ItemStatus.check_time, ItemStatus.item_detail).where(
+#             ItemStatus.item_id == item_id, ItemStatus.check_time <= end_date + ' 23:59:59',
+#             ItemStatus.check_time >= start_date).order_by(ItemStatus.check_time.asc()).dicts()
+#         if datetime.strptime(end_date.strip(), '%Y/%m/%d') - timedelta(days=2) < datetime.strptime(start_date.strip(), '%Y/%m/%d'):
+#             date_time = query[0]['check_time'].strftime('%Y-%m-%d %H')
+#             for data in query:
+#                 check_time = data['check_time'].strftime('%Y-%m-%d %H')
+#                 status = utils.str_to_dict(data['item_detail'])
+#                 if date_time == check_time:
+#                     if 'status' in status:
+#                         status_list.append(status.get('status'))
+#                     else:
+#                         rx_speed_list.append(status.get('rx_speed'))
+#                         tx_speed_list.append(status.get('tx_speed'))
+#                 else:
+#                     if status_list:
+#                         avg_list.append(sum(status_list) / len(status_list))
+#                         status_list = [status.get('status')]
+#                     else:
+#                         avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
+#                         avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
+#                         rx_speed_list = [status.get('rx_speed')]
+#                         tx_speed_list = [status.get('tx_speed')]
+#                     date_list.append(date_time)
+#                     date_time = check_time
+#         else:
+#             date_time = query[0]['check_time'].strftime('%Y-%m-%d')
+#             for data in query:
+#                 check_time = data['check_time'].strftime('%Y-%m-%d')
+#                 status = utils.str_to_dict(data['item_detail'])
+#                 if date_time == check_time:
+#                     if status.get('status'):
+#                         status_list.append(status.get('status'))
+#                     else:
+#                         rx_speed_list.append(status.get('rx_speed'))
+#                         tx_speed_list.append(status.get('tx_speed'))
+#                 else:
+#                     if status_list:
+#                         avg_list.append(sum(status_list) / len(status_list))
+#                         status_list = [status.get('status')]
+#                     else:
+#                         avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
+#                         avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
+#                         rx_speed_list = [status.get('rx_speed')]
+#                         tx_speed_list = [status.get('tx_speed')]
+#                     date_list.append(date_time)
+#                     date_time = check_time
+#         date_list.append(date_time)
+#         if status_list:
+#             avg_list.append(sum(status_list) / len(status_list))
+#         else:
+#             avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
+#             avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
+#         return draw_echarts(query[0].get('item_name'), warning_value, date_list, avg_list, avg_rx_list, avg_tx_list)
+
+
+# 获取项目状态图表
 def get_item_echarts(rq):
     if rq.method == 'POST':
         status_list = []
         rx_speed_list = []
         tx_speed_list = []
         date_list = []
-        avg_list = []
-        avg_rx_list = []
-        avg_tx_list = []
         item_id = rq.form.get('item_id')
         date_ware = rq.form.get('date_ware')
         warning_value = rq.form.get('warning_value')
@@ -28,57 +95,15 @@ def get_item_echarts(rq):
         query = ItemStatus.select(ItemStatus.item_name, ItemStatus.check_time, ItemStatus.item_detail).where(
             ItemStatus.item_id == item_id, ItemStatus.check_time <= end_date + ' 23:59:59',
             ItemStatus.check_time >= start_date).order_by(ItemStatus.check_time.asc()).dicts()
-        if datetime.strptime(end_date.strip(), '%Y/%m/%d') - timedelta(days=2) < datetime.strptime(start_date.strip(), '%Y/%m/%d'):
-            date_time = query[0]['check_time'].strftime('%Y-%m-%d %H')
-            for data in query:
-                check_time = data['check_time'].strftime('%Y-%m-%d %H')
-                status = utils.str_to_dict(data['item_detail'])
-                if date_time == check_time:
-                    if 'status' in status:
-                        status_list.append(status.get('status'))
-                    else:
-                        rx_speed_list.append(status.get('rx_speed'))
-                        tx_speed_list.append(status.get('tx_speed'))
-                else:
-                    if status_list:
-                        avg_list.append(sum(status_list) / len(status_list))
-                        status_list = [status.get('status')]
-                    else:
-                        avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
-                        avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
-                        rx_speed_list = [status.get('rx_speed')]
-                        tx_speed_list = [status.get('tx_speed')]
-                    date_list.append(date_time)
-                    date_time = check_time
-        else:
-            date_time = query[0]['check_time'].strftime('%Y-%m-%d')
-            for data in query:
-                check_time = data['check_time'].strftime('%Y-%m-%d')
-                status = utils.str_to_dict(data['item_detail'])
-                if date_time == check_time:
-                    if status.get('status'):
-                        status_list.append(status.get('status'))
-                    else:
-                        rx_speed_list.append(status.get('rx_speed'))
-                        tx_speed_list.append(status.get('tx_speed'))
-                else:
-                    if status_list:
-                        avg_list.append(sum(status_list) / len(status_list))
-                        status_list = [status.get('status')]
-                    else:
-                        avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
-                        avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
-                        rx_speed_list = [status.get('rx_speed')]
-                        tx_speed_list = [status.get('tx_speed')]
-                    date_list.append(date_time)
-                    date_time = check_time
-        date_list.append(date_time)
-        if status_list:
-            avg_list.append(sum(status_list) / len(status_list))
-        else:
-            avg_rx_list.append(sum(rx_speed_list) / len(rx_speed_list))
-            avg_tx_list.append(sum(tx_speed_list) / len(tx_speed_list))
-        return draw_echarts(query[0].get('item_name'), warning_value, date_list, avg_list, avg_rx_list, avg_tx_list)
+        for data in query:
+            status = utils.str_to_dict(data['item_detail'])
+            date_list.append(data['check_time'])
+            if 'status' in status:
+                status_list.append(status.get('status'))
+            else:
+                rx_speed_list.append(status.get('rx_speed'))
+                tx_speed_list.append(status.get('tx_speed'))
+        return draw_echarts(query[0].get('item_name'), warning_value, date_list, status_list, rx_speed_list, tx_speed_list)
 
 
 def format_num(num):
@@ -86,18 +111,18 @@ def format_num(num):
 
 
 # 绘制echarts图表
-def draw_echarts(item_name, warning_value, date_list, avg_list=[], avg_rx_list=[], avg_tx_list=[]):
-    assert isinstance(avg_list, list)
-    assert isinstance(avg_rx_list, list)
-    assert isinstance(avg_tx_list, list)
+def draw_echarts(item_name, warning_value, date_list, status_list=[], rx_speed_list=[], tx_speed_list=[]):
+    assert isinstance(status_list, list)
+    assert isinstance(rx_speed_list, list)
+    assert isinstance(tx_speed_list, list)
 
     area = Line()
     area.add_xaxis(date_list)
-    if avg_list:
-        area.add_yaxis("使用率（%）", [data for data in map(format_num, avg_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
+    if status_list:
+        area.add_yaxis("使用率（%）", [data for data in map(format_num, status_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
     else:
-        area.add_yaxis("rx（接收速率KB/s）", [data for data in map(format_num, avg_rx_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
-        area.add_yaxis("tx（发送速率KB/s）", [data for data in map(format_num, avg_tx_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
+        area.add_yaxis("rx（接收速率KB/s）", [data for data in map(format_num, rx_speed_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
+        area.add_yaxis("tx（发送速率KB/s）", [data for data in map(format_num, tx_speed_list)], areastyle_opts=opts.AreaStyleOpts(opacity=0.5))
     area.set_global_opts(title_opts=opts.TitleOpts(title=item_name, pos_left="10%"),
                          legend_opts=opts.LegendOpts(pos_left="30%"))
     area.set_series_opts(
@@ -105,6 +130,8 @@ def draw_echarts(item_name, warning_value, date_list, avg_list=[], avg_rx_list=[
             data=[opts.MarkLineItem(y=warning_value, name='基准值')]
         )
     )
+    if len(date_list) > 20:
+        area.set_global_opts(datazoom_opts=opts.DataZoomOpts(range_start=10, range_end=30))
 
     grid = Grid(init_opts=opts.InitOpts(width="100%"))
     grid.add(area, grid_opts=opts.GridOpts())
